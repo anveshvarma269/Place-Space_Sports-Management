55a1bc918d25fa6bb15c209ee6727a90a05ace19
VishnuTeja4496 <pakalapati.v@northeastern.edu>
2021-12-10 16:17:54 -0500

    Additional Roles

---
+++ PlaySpace/src/Sports/role/DataAnalystRole.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sports.role;
+
+import Sports.EcoSystem;
+import Sports.Enterprise.Enterprise;
+import Sports.Organization.Organization;
+import Sports.User.UserAccount;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author vishnuteja
+ */
+public class DataAnalystRole {
+     @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
