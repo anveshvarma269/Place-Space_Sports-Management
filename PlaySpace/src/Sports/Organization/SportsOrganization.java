d348e30f68e58ae9049ed07a49c08bae94d0df55
revanth3101 <gudavarthy.r@northeastern.edu>
2021-12-10 15:58:23 -0500

    organization

---
+++ PlaySpace/src/Sports/Organization/SportsOrganization.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Sports.Organization;
+
+import Sports.Role.Role;
+import Sports.Sport.SportDirectory;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author revanthkumar
+ */
+public class SportsOrganization extends Organization { 
+
+     private SportDirectory sportDirectory;
+
+    public SportDirectory getSportDirectory() {
+        return sportDirectory;
+    }
+
+    public void setSportDirectory(SportDirectory sportDirectory) {
+        this.sportDirectory = sportDirectory;
+    }
+
+    public SportsOrganization()
+    {
+        super(Organization.Type.Sports.getValue());
+sportDirectory = new SportDirectory();
+    }
+    
+    
+    
+    
+      @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList<>();
+        roles.add(new SystemAdminRole());
+        return roles;
+    }
+}
---
+++ PlaySpace/src/Sports/role/SystemAdminRole.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Sports.role;
+
+import Sports.EcoSystem;
+import Sports.Enterprise.Enterprise;
+import Sports.Organization.Organization;
+import Sports.Role.Role;
+import Sports.User.UserAccount;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author revanthkumar
+ */
+public class SystemAdminRole extends Role{
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+}
